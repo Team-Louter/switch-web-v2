c2e0fc6fb7b6c983ab3449ab2bb9015c117d5dc5
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-07-28 20:15:54 +0900

    feat : 학습 관리 페이지 구현

--- src/pages/learning/ui/LearningPage.tsx
+++ src/pages/learning/ui/LearningPage.tsx
@@ -1,9 +1,62 @@
-import { Container, Text } from '@/pages/temp-page.style'
+import * as S from './LearningPage.style.ts'
+import { PercentageBar } from './PercentageBar/PercentageBar.tsx'
+import { MonthlyJournalWeeks } from './MonthlyJournalWeeks/MonthlyJournalWeeks.tsx'
+import decoImg1 from '../assets/deco1.svg'
+import decoImg2 from '../assets/spring.svg'
+import {
+  getCurrentMonth,
+  getMonthsFromCurrentMonth,
+  getMonthState,
+} from '../lib/getMonthsFromCurrentMonth.ts'
 
 export function LearningPage() {
+  const submitRate = 0
+  const currentMonth = getCurrentMonth()
+
   return (
-    <Container>
-      <Text>학습관리 임시 화면입니다</Text>
-    </Container>
+    <S.PageContainer>
+      <S.ScrollArea>
+        {getMonthsFromCurrentMonth().map((month) => {
+          const monthState = getMonthState(month, currentMonth)
+
+          return (
+          <S.Column key={month} $state={monthState}>
+            <S.MonthRow>
+              <S.Month>{month}월</S.Month>
+              {monthState === 'current' && <S.Now>Now</S.Now>}
+            </S.MonthRow>
+            <S.Card $state={monthState}>
+              <S.ProgressContent>
+                <S.SubmitLabel>제출</S.SubmitLabel>
+                <S.SubmitRate>{submitRate}%</S.SubmitRate>
+                <PercentageBar value={submitRate} label="과제 제출률" />
+                <S.Status>진행중</S.Status>
+              </S.ProgressContent>
+              <S.DiaryContent>
+                <MonthlyJournalWeeks
+                  weeks={[
+                    { week: 1, status: 'submitted' },
+                    { week: 2, status: 'overdue' },
+                    { week: 3, status: 'due' },
+                    { week: 4, status: 'locked' },
+                    { week: 5, status: 'locked' },
+                  ]}
+                />
+                <S.DecoImg src={decoImg1} alt="" />
+                <S.ButtonContent>
+                  <S.Name>2213 최현수</S.Name>
+                  <S.Week>3주차 학습일지</S.Week>
+                  <S.WriteButton type="button">
+                    작성하기
+                  </S.WriteButton>
+                </S.ButtonContent>
+              </S.DiaryContent>
+              <S.DecoImg2 src={decoImg2} alt="" />
+            </S.Card>
+          </S.Column>
+          )
+        })}
+      </S.ScrollArea>
+    </S.PageContainer>
   )
 }
