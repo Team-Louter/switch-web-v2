1ebbd365c11c55cdf814090fe5b2894647b6c92a
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-08-03 21:17:11 +0900

    feat: 학습일지 수정 API 추가

--- src/features/study/api/createStudy.ts
+++ src/features/study/api/createStudy.ts
@@ -1,7 +1,11 @@
 import { apiClient } from '@/shared/api'
 
-import type { CreateStudyRequest } from '../model/types'
+import type { CreateStudyRequest, StudyRecord } from '../model/types'
 
 export const createStudy = async (data: CreateStudyRequest): Promise<void> => {
   await apiClient.post<void>('/studies', data);
+}
+
+export const modifyStudy = async (studyId: number, data: CreateStudyRequest): Promise<void> => {
+  await apiClient.put<StudyRecord>(`/studies/${studyId}`, data);
 }
