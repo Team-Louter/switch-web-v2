10ddb46f3365dfb7d3425962df13c994a856a9d1
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-10 23:08:29 +0900

    refactor : 로그인 인트로 공용 컴포넌트 적용

--- src/pages/auth/ui/LoginPanel.tsx
+++ src/pages/auth/ui/LoginPanel.tsx
@@ -1,5 +1,5 @@
-import louterLogoImage from '../assets/images/louter-logo.png'
 import type { LoginFormController } from '../model/useLoginForm'
+import { AuthIntro } from './AuthIntro'
 import { LoginForm } from './LoginForm'
 import * as S from './LoginPanel.style'
 
@@ -11,14 +11,7 @@ export function LoginPanel({ controller }: LoginPanelProps) {
   return (
     <S.Panel $isPasswordStep={controller.isPasswordStep}>
       <S.Content>
-        <S.Intro>
-          <S.PartnerLogo src={louterLogoImage} alt="" />
-          <S.IntroCopy>
-            <S.Title id="login-title">Louter (라우터)</S.Title>
-            <S.Subtitle>로그인 및 회원가입</S.Subtitle>
-          </S.IntroCopy>
-        </S.Intro>
-
+        <AuthIntro titleId="login-title" />
         <LoginForm controller={controller} />
       </S.Content>
     </S.Panel>
