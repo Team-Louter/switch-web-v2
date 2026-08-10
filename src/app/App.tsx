0fb6b8254d4a0a1889906e81266a31128279197d
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-10 21:59:37 +0900

    feat : 로그인 접근 제어 라우팅 적용

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,22 +15,28 @@ import {
 
 import { AppLayout } from './layouts'
 import { AppProvider } from './providers'
+import { GuestOnlyRoute, ProtectedRoute, RootRoute } from './router'
 
 export function App() {
   return (
     <AppProvider>
       <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route element={<AppLayout />}>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/community" element={<CommunityPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/learning" element={<LearningPage />} />
-          <Route path="/mentoring" element={<MentoringPage />} />
-          <Route path="/typing" element={<TypingPage />} />
-          <Route path="/notification" element={<NotificationPage />} />
-          <Route path="/store" element={<StorePage />} />
-          <Route path="/my" element={<MyPage />} />
+        <Route path="/" element={<RootRoute />} />
+        <Route element={<GuestOnlyRoute />}>
+          <Route path="/login" element={<AuthPage />} />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/learning" element={<LearningPage />} />
+            <Route path="/mentoring" element={<MentoringPage />} />
+            <Route path="/typing" element={<TypingPage />} />
+            <Route path="/notification" element={<NotificationPage />} />
+            <Route path="/store" element={<StorePage />} />
+            <Route path="/my" element={<MyPage />} />
+          </Route>
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
