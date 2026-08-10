2995a65fa36308bf9ea89e9aa82dea478bba64f6
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-10 22:00:22 +0900

    feat : Switch 로고 로그인 링크 적용

--- src/pages/auth/ui/AuthHeader.tsx
+++ src/pages/auth/ui/AuthHeader.tsx
@@ -6,7 +6,9 @@ export function AuthHeader() {
   return (
     <S.Header>
       <S.BrandGroup>
-        <S.SwitchLogo src={switchLogo} alt="Switch" />
+        <S.SwitchLink to="/login">
+          <S.SwitchLogo src={switchLogo} alt="Switch" />
+        </S.SwitchLink>
         <S.CollaborationMark aria-hidden="true">X</S.CollaborationMark>
         <S.PartnerLogo src={louterLogoImage} alt="Louter" />
       </S.BrandGroup>
