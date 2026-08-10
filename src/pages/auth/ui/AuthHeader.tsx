0d3870ba15fdcfefd32c61e8ebe6f3ff2c090cc1
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-10 22:53:46 +0900

    refactor : Switch 클릭 콜백 지원

--- src/pages/auth/ui/AuthHeader.tsx
+++ src/pages/auth/ui/AuthHeader.tsx
@@ -2,11 +2,15 @@ import louterLogoImage from '../assets/images/louter-logo.png'
 import switchLogo from '../assets/svg/switch-logo.svg'
 import * as S from './AuthHeader.style'
 
-export function AuthHeader() {
+interface AuthHeaderProps {
+  onSwitchClick?: () => void
+}
+
+export function AuthHeader({ onSwitchClick }: AuthHeaderProps) {
   return (
     <S.Header>
       <S.BrandGroup>
-        <S.SwitchLink to="/login">
+        <S.SwitchLink to="/login" onClick={onSwitchClick}>
           <S.SwitchLogo src={switchLogo} alt="Switch" />
         </S.SwitchLink>
         <S.CollaborationMark aria-hidden="true">X</S.CollaborationMark>
