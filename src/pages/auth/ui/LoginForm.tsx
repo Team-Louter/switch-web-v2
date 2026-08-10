00be53b224bf1daffd12a5fc9ee3a796db220873
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-10 21:45:10 +0900

    refactor : 로그인 입력 폼 컴포넌트 분리

---
+++ src/pages/auth/ui/LoginForm.tsx
@@ -0,0 +1,118 @@
+import { Turnstile } from '@/features/auth'
+
+import type { LoginFormController } from '../model/useLoginForm'
+import googleLogo from '../assets/svg/google-logo.svg'
+import * as S from './LoginForm.style'
+
+interface LoginFormProps {
+  controller: LoginFormController
+}
+
+export function LoginForm({ controller }: LoginFormProps) {
+  const {
+    email,
+    password,
+    isPasswordStep,
+    isContinueDisabled,
+    turnstileSiteKey,
+    handleEmailChange,
+    handlePasswordChange,
+    handleContinue,
+    handleChangeEmail,
+    handleTurnstileVerify,
+    handleTurnstileReset,
+  } = controller
+
+  return (
+    <>
+      <S.Options>
+        <S.GoogleButton type="button">
+          <S.GoogleLogo src={googleLogo} alt="" />
+          Google으로 계속하기
+        </S.GoogleButton>
+
+        <S.Divider aria-hidden="true">
+          <S.DividerLine />
+          <S.DividerText>또는</S.DividerText>
+          <S.DividerLine />
+        </S.Divider>
+
+        <S.EmailGroup>
+          <S.Fields>
+            <S.EmailField>
+              <S.EmailInput
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleEmailChange}
+                aria-label="이메일"
+                placeholder="이메일을 입력해주세요"
+                autoComplete="email"
+                readOnly={isPasswordStep}
+              />
+              <S.ChangeEmailButton
+                type="button"
+                $isVisible={isPasswordStep}
+                onClick={handleChangeEmail}
+                aria-hidden={!isPasswordStep}
+                tabIndex={isPasswordStep ? 0 : -1}
+              >
+                변경
+              </S.ChangeEmailButton>
+            </S.EmailField>
+
+            <S.PasswordFieldSlot
+              $isVisible={isPasswordStep}
+              aria-hidden={!isPasswordStep}
+            >
+              <S.PasswordFieldMotion $isVisible={isPasswordStep}>
+                <S.PasswordInput
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  aria-label="비밀번호"
+                  placeholder="비밀번호를 입력해주세요"
+                  autoComplete="current-password"
+                  disabled={!isPasswordStep}
+                  tabIndex={isPasswordStep ? 0 : -1}
+                />
+              </S.PasswordFieldMotion>
+            </S.PasswordFieldSlot>
+          </S.Fields>
+
+          {turnstileSiteKey ? (
+            <Turnstile
+              key="turnstile"
+              siteKey={turnstileSiteKey}
+              onVerify={handleTurnstileVerify}
+              onExpire={handleTurnstileReset}
+              onError={handleTurnstileReset}
+            />
+          ) : (
+            <S.TurnstileConfigMessage role="alert">
+              보안 인증 설정이 필요합니다
+            </S.TurnstileConfigMessage>
+          )}
+        </S.EmailGroup>
+      </S.Options>
+
+      <S.ActionArea>
+        <S.ContinueButton
+          type="button"
+          disabled={isContinueDisabled}
+          onClick={handleContinue}
+        >
+          계속
+        </S.ContinueButton>
+        <S.Footer>
+          <S.PolicyLinks>
+            <span>서비스 이용약관</span>
+            <span>개인정보 처리방침</span>
+          </S.PolicyLinks>
+          <S.Team>Team. Louter</S.Team>
+        </S.Footer>
+      </S.ActionArea>
+    </>
+  )
+}
