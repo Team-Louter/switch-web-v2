367bbffc2778b7faf1721ada91261d13e3a253ea
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-11 10:36:00 +0900

    feat : login 내부 회원가입 화면 전환

--- src/pages/auth/ui/AuthPage.tsx
+++ src/pages/auth/ui/AuthPage.tsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import { AuthHeader } from './AuthHeader'
 import * as S from './AuthPage.style'
 import { LoginCard } from './LoginCard'
+import { SignupPage } from './SignupPage'
+
+function isSignupView(locationState: unknown): boolean {
+  return (
+    typeof locationState === 'object' &&
+    locationState !== null &&
+    'authView' in locationState &&
+    locationState.authView === 'signup'
+  )
+}
 
 export function AuthPage() {
+  const location = useLocation()
   const [loginCardKey, setLoginCardKey] = useState(0)
 
   function handleLoginReset() {
     setLoginCardKey((currentKey) => currentKey + 1)
+  }
+
+  if (isSignupView(location.state)) {
+    return <SignupPage />
   }
 
   return (
