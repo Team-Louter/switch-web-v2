579c97fd30ae820429d66b83d17d1b8f05f2f3fc
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-08-11 15:19:39 +0900

    feat: 타자 연습 완료 결과 모달 UI 구현

--- src/features/typing/index.ts
+++ src/features/typing/index.ts
@@ -1,2 +1,3 @@
 export { TypingSentenceModal } from './ui/TypingSentenceModal/TypingSentenceModal'
 export type { TypingSentenceModalType } from './ui/TypingSentenceModal/TypingSentenceModal'
+export { TypingCompletionModal } from './ui/TypingCompletionModal/TypingCompletionModal'
