7493acf6f0c27fc26570cedb0c9917ddd9bc3e03
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-11 10:36:58 +0900

    refactor : 페이지 공개 API 정리

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { AuthPage, SignupPage } from './auth'
+export { AuthPage } from './auth'
 export { CalendarPage } from './calendar'
 export { CommunityPage } from './community'
 export { HomePage } from './home'
