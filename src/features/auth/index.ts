cf1f73ed3c0124b1cd4a2cb5cb9157aefe138d02
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-11 16:08:19 +0900

    feat : 회원가입 인증 API 공개

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -5,5 +5,17 @@ export type {
   LoginRequest,
   LoginResponse,
 } from './api/login'
+export { sendVerificationCode } from './api/sendVerificationCode'
+export type {
+  SendVerificationCodeRequest,
+  SendVerificationCodeResponse,
+} from './api/sendVerificationCode'
+export { signup } from './api/signup'
+export type { SignupRequest, SignupResponse } from './api/signup'
+export { verifyEmailCode } from './api/verifyEmailCode'
+export type {
+  VerifyEmailCodeRequest,
+  VerifyEmailCodeResponse,
+} from './api/verifyEmailCode'
 export { startGoogleLogin } from './lib/startGoogleLogin'
 export { Turnstile } from './ui/Turnstile/Turnstile'
