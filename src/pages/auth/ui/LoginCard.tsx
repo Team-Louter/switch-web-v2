9c523aef29929d40c32a420ad5e8c6f883684666
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-11 13:47:42 +0900

    feat : 로그인 화면 고화질 이미지 적용

--- src/pages/auth/ui/LoginCard.tsx
+++ src/pages/auth/ui/LoginCard.tsx
@@ -1,4 +1,4 @@
-import loginHeroImage from '../assets/images/login-hero.png'
+import authHeroImage from '../assets/images/auth-hero.jpg'
 import { useLoginForm } from '../model/useLoginForm'
 import * as S from './LoginCard.style'
 import { LoginPanel } from './LoginPanel'
@@ -53,7 +53,7 @@ export function LoginCard({
     >
       <S.Hero $heightOffset={heightOffset}>
         <S.HeroImage
-          src={loginHeroImage}
+          src={authHeroImage}
           alt="Louter 캐릭터들이 함께 노는 모습"
         />
       </S.Hero>
