4753508e0fdec5900be64c265a180c7a9d0764d0
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-12 15:46:54 +0900

    style : 알림 콘텐츠 가로 폭 축소

--- src/pages/notification/ui/NotificationPage.style.ts
+++ src/pages/notification/ui/NotificationPage.style.ts
@@ -15,7 +15,7 @@ export const Page = styled.main`
 
 export const Content = styled.div`
   ${token.flexColumnStart}
-  width: min(100%, 920px);
+  width: min(100%, 760px);
   gap: 32px;
 
   @media (max-width: 760px) {
