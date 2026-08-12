318439f43f63b7429863824821ba6f94260366ed
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-12 12:25:38 +0900

    feat : 알림 변경 공개 API 연결

--- src/features/notification/index.ts
+++ src/features/notification/index.ts
@@ -1,7 +1,15 @@
+export { deleteNotification } from './api/deleteNotification'
+export {
+  readAllNotifications,
+  readNotification,
+} from './api/readNotifications'
+export { updateNotificationSettings } from './api/updateNotificationSettings'
 export type {
   NotificationMenuAction,
   NotificationSettingKey,
   NotificationSettings,
+  UpdateNotificationChannelSettingsRequest,
+  UpdateNotificationContentSettingsRequest,
 } from './model/types'
 export {
   DeleteNotificationModal,
