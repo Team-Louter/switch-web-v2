f05dd71f091db2ad38eb1df0095e34ce7f67393e
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-12 12:23:55 +0900

    feat : 알림 조회 공개 API 연결

--- src/entities/notification/index.ts
+++ src/entities/notification/index.ts
@@ -1,6 +1,11 @@
+export { getNotificationSettings } from './api/getNotificationSettings'
+export { getNotifications } from './api/getNotifications'
+export { mapNotificationResponse } from './lib/mapNotificationResponse'
 export type {
   Notification,
-  NotificationListResponse,
+  NotificationApiType,
+  NotificationResponse,
+  NotificationSettingsResponse,
   NotificationType,
 } from './model/types'
 export { NotificationItem } from './ui/NotificationItem'
