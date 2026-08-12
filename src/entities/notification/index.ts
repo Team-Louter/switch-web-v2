512649fb2179adeab600ffc70eaa7ca9cc998576
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-12 22:00:30 +0900

    feat : 알림 조회 공개 API 확장

--- src/entities/notification/index.ts
+++ src/entities/notification/index.ts
@@ -1,11 +1,20 @@
 export { getNotificationSettings } from './api/getNotificationSettings'
 export { getNotifications } from './api/getNotifications'
+export { getUnreadNotificationCount } from './api/getUnreadNotificationCount'
 export { mapNotificationResponse } from './lib/mapNotificationResponse'
 export type {
+  GetNotificationsParams,
   Notification,
+  NotificationActorResponse,
   NotificationApiType,
+  NotificationPageResponse,
+  NotificationPageableResponse,
   NotificationResponse,
   NotificationSettingsResponse,
+  NotificationSortResponse,
+  NotificationTargetResponse,
+  NotificationTargetType,
   NotificationType,
+  UnreadNotificationCountResponse,
 } from './model/types'
 export { NotificationItem } from './ui/NotificationItem'
