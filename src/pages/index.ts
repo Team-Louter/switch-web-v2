72b8a92d297db0395f62828a980ce6f92a426b6f
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-13 23:30:24 +0900

    feat : 상세 페이지 배럴 내보내기 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,7 +4,7 @@ export {
   GoogleOAuthCallbackPage,
 } from './auth'
 export { CalendarPage } from './calendar'
-export { CommunityPage } from './community'
+export { CommunityDetailPage, CommunityPage } from './community'
 export { HomePage } from './home'
 export { LearningPage } from './study'
 export { MentoringPage } from './mentoring'
