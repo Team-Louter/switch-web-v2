e7358314bd599814d92ba7d6da715626db13f405
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-14 01:06:50 +0900

    style : 게시글 상세 요소 크기 축소

--- src/pages/community/ui/CommunityDetailPage.style.ts
+++ src/pages/community/ui/CommunityDetailPage.style.ts
@@ -19,9 +19,10 @@ export const Page = styled.section`
 export const Content = styled.div`
   ${token.flexColumn}
   gap: 40px;
-  width: 100%;
-  max-width: 1003px;
+  width: calc(100% / 0.9);
+  max-width: calc(1003px / 0.9);
   margin: 0 auto;
+  zoom: 0.9;
 
   @container community-detail (max-width: 700px) {
     gap: 28px;
