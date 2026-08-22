581b308d8416cffa2a94dfc9e33e70f488e4ad70
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-22 21:54:03 +0900

    feat : 회원 역할 타입 추가

--- src/entities/member/model/types.ts
+++ src/entities/member/model/types.ts
@@ -1,9 +1,15 @@
+export type MemberRole = 'LEADER' | 'MENTOR' | 'MENTEE' | 'STUDENT'
+
+export interface CurrentMember {
+  role: MemberRole
+}
+
 export interface Member {
   userId: number,
   userName: string,
   profileImageUrl: string,
   majors: string[],
-  role: string,
+  role: MemberRole,
   generation: number,
   githubUrl: string,
   linkedinUrl: string,
