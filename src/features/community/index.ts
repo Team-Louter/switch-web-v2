2143da1fa930132b7b04e4903d5b93528519ebab
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-22 22:43:38 +0900

    feat : 게시글 수정 삭제 기능 공개

--- src/features/community/index.ts
+++ src/features/community/index.ts
@@ -1,8 +1,10 @@
 export {
   createComment,
   createPost,
+  deletePost,
   setPostPinned,
   togglePostHeart,
+  updatePost,
   uploadCommunityFile,
 } from './api/communityActions'
 export type {
