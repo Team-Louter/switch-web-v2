3035572d087f894d0799ae0ca44a7e123252a1c7
hyxx-su <legendary9899@dgsw.hs.kr>
2026-08-22 21:41:41 +0900

    feat : 게시글 파일 URL 유틸 공개

--- src/entities/community/index.ts
+++ src/entities/community/index.ts
@@ -6,6 +6,7 @@ export {
 } from './api/getCommunity'
 export {
   formatCommunityDate,
+  getCommunityFileDownloadUrl,
   getPostCategoryLabel,
   POST_CATEGORY_OPTIONS,
   resolveCommunityAssetUrl,
