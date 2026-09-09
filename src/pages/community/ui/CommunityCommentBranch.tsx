9a0a1f40a8cad99e8ca80066df80f2bd830dbdbd
hyxx-su <legendary9899@dgsw.hs.kr>
2026-09-10 08:59:34 +0900

    fix : 커뮤니티 깊은 답글 평탄화 기준 조정

--- src/pages/community/ui/CommunityCommentBranch.tsx
+++ src/pages/community/ui/CommunityCommentBranch.tsx
@@ -24,7 +24,7 @@ import {
 } from './communityCommentTree'
 import * as S from './CommunityCommentBranch.style'
 
-const FLATTENED_TREE_DEPTH = 5
+const FLATTENED_TREE_DEPTH = 4
 
 interface ReplyLoadingSkeletonProps {
   isWithinReplies?: boolean
