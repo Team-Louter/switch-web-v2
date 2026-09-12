ca100fa1e382a3397a215b30614b97b0e6ac394e
hyxx-su <legendary9899@dgsw.hs.kr>
2026-09-12 14:52:58 +0900

    style : 토스트 자동 닫힘 시간 단축

--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -25,7 +25,7 @@ export function AppProvider({ children }: PropsWithChildren) {
       <GlobalStyle />
       <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick={false}
